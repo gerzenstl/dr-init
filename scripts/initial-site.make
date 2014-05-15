4c492fd690ba586fdb33c1ccacf4504684d3da19
Luis <gerzenstl@gmail.com>
2014-05-14 22:35:14 -0300

    Initial commit

---
+++ scripts/initial-site.make
@@ -0,0 +1,104 @@
+
+; This is a working makefile - try it! Any line starting with a `;` is a comment.
+  
+; Core version
+; ------------
+; Each makefile should begin by declaring the core version of Drupal that all
+; projects should be compatible with.
+  
+core = 7.x
+  
+; API version
+; ------------
+; Every makefile needs to declare its Drush Make API version. This version of
+; drush make uses API version `2`.
+  
+api = 2
+  
+; Core project
+; ------------
+; In order for your makefile to generate a full Drupal site, you must include
+; a core project. This is usually Drupal core, but you can also specify
+; alternative core projects like Pressflow. Note that makefiles included with
+; install profiles *should not* include a core project.
+  
+; Drupal 7.x. Requires the `core` property to be set to 7.x.
+projects[drupal][version] = 7
+  
+; Modules
+; --------
+projects[admin][type] = "module"
+projects[admin][subdir] = "contrib"
+projects[admin_menu][type] = "module"
+projects[admin_menu][subdir] = "contrib"
+projects[admin_theme][type] = "module"
+projects[admin_theme][subdir] = "contrib"
+projects[backup_migrate][type] = "module"
+projects[backup_migrate][subdir] = "contrib"
+projects[ctools][type] = "module"
+projects[ctools][subdir] = "contrib"
+projects[context][type] = "module"
+projects[context][subdir] = "contrib"
+projects[calendar][type] = "module"
+projects[calendar][subdir] = "contrib"
+projects[date][type] = "module"
+projects[date][subdir] = "contrib"
+projects[devel][type] = "module"
+projects[devel][subdir] = "contrib"
+projects[diff][type] = "module"
+projects[diff][subdir] = "contrib"
+projects[elysia_cron][type] = "module"
+projects[elysia_cron][subdir] = "contrib"
+projects[entity][type] = "module"
+projects[entity][subdir] = "contrib"
+projects[entityreference][type] = "module"
+projects[entityreference][subdir] = "contrib"
+projects[features][type] = "module"
+projects[features][subdir] = "contrib"
+projects[field_group][type] = "module"
+projects[field_group][subdir] = "contrib"
+projects[fivestar][type] = "module"
+projects[fivestar][subdir] = "contrib"
+projects[google_analytics][type] = "module"
+projects[google_analytics][subdir] = "contrib"
+projects[jquery_update][type] = "module"
+projects[jquery_update][subdir] = "contrib"
+projects[imce][type] = "module"
+projects[imce][subdir] = "contrib"
+projects[imce_wysiwyg][type] = "module"
+projects[imce_wysiwyg][subdir] = "contrib"
+projects[i18n][type] = "module"
+projects[i18n][subdir] = "contrib"
+projects[libraries][type] = "module"
+projects[libraries][subdir] = "contrib"
+projects[l10n_update][type] = "module"
+projects[l10n_update][subdir] = "contrib"
+projects[nodequeue][type] = "module"
+projects[nodequeue][subdir] = "contrib"
+projects[pathauto][type] = "module"
+projects[pathauto][subdir] = "contrib"
+projects[panels][type] = "module"
+projects[panels][subdir] = "contrib"
+projects[strongarm][type] = "module"
+projects[strongarm][subdir] = "contrib"
+projects[token][type] = "module"
+projects[token][subdir] = "contrib"
+projects[variable][type] = "module"
+projects[variable][subdir] = "contrib"
+projects[views][type] = "module"
+projects[views][subdir] = "contrib"
+projects[wysiwyg][type] = "module"
+projects[wysiwyg][subdir] = "contrib"
+
+  
+
+; Themes
+; --------
+
+  
+  
+; Libraries
+; ---------
+; No libraries were included
+
+
---
+++ scripts/install-drupal.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+# Script to automates Drupal site installation.
+
+if [ $# -lt 1 ]
+then
+  echo ""
+  echo "USAGE: $0 [Site-name]"
+  echo "Site-Name is the non-fully qualified site name such as connect or euconsumer."
+  echo "Use letters and '_' (underscore) chars for site-name."
+  echo ""
+  exit
+fi
+
+SITENAME=$1
+
+if [[ $SITENAME =~ ^[A-Za-z_]+$ ]]; then
+  echo "Valid Site-name"  
+else
+  echo ""
+  echo "ERROR: Unvalid site-name. Remember to use letters and '_' (underscore) chars for site-name."
+  echo ""
+  exit
+fi
+
+echo ""
+echo "**************************************************"
+echo "**************************************************"
+echo " WARNING: Did you setup the Env & Site variables? "
+echo "**************************************************"
+echo "**************************************************"
+echo ""
+read ANSWER1
+if [ "$ANSWER1" == 'y' ] || [ "$ANSWER1" == 'Y' ]; then
+  echo "Cool! Lets continue with the installation."
+else
+  echo "Please, setup variables and start again."
+  exit
+fi
+exit
+
+
+# Directories
+##########################################################
+ROOTDIR="src" 
+
+ 
+# Site
+##########################################################
+SITESLOGAN=""
+SITELOCALE="es"
+
+ 
+# Environment Database settings
+##########################################################
+ENVDBHOST="localhost"
+ENVDBUSER="root"
+ENVDBPASSWORD="root"
+
+
+# Drupal Database settings
+##########################################################
+DBHOST="localhost"
+DBNAME="codigociudadano"
+DBUSER="codigociudadano"
+DBPASSWORD="codigociudadano"
+
+ 
+# Admin
+##########################################################
+ADMINUSERNAME="root"
+ADMINPASSWORD="123"
+ADMINEMAIL="$ADMINUSERNAME@$SITENAME.com"
+
+
+# DB Setup
+##########################################################
+echo ""
+echo "Setting up site database..."
+
+RESULT=`mysqlshow -u$ENVDBUSER -p$ENVDBPASSWORD $DBNAME| grep -v Wildcard | grep -o $DBNAME`
+if [ "$RESULT" == $DBNAME ]; then
+ echo "ERROR: The chosen DB already exists. Please check the settings."
+ echo "Exiting."
+ exit
+else
+  mysql -u$DBUSER -p$DBPASSWORD -e"CREATE DATABASE $DBNAME"
+fi
+
+echo "Creating db user for $DBNAME"
+mysql -u$ENVDBUSER -p$ENVDBPASSWORD -e"GRANT ALL ON $DBNAME.* TO '$DBUSER'@'$DBHOST' IDENTIFIED BY '$DBPASSWORD'"
+
+
+# Download Core
+##########################################################
+echo "Making Drupal based on profile..."
+
+drush make initial-site.make ../$ROOTDIR --prepare-install
+ 
+cd ../$ROOTDIR;
+ 
+
+# Install core
+##########################################################
+echo "Installing Drupal core..."
+drush site-install -y standard --account-mail=$ADMINEMAIL --account-name=$ADMINUSERNAME --account-pass=$ADMINPASSWORD --site-name=$SITENAME --site-mail=$ADMINEMAIL --locale=$SITELOCALE --db-url=mysql://$DBUSER:$DBPASSWORD@$DBHOST/$DBNAME;
+
+# Enabling modules
+##########################################################
+DFLTFOLDERPATH="$ROOTDIR/sites/default"
+
+cp modules_enabled.txt ../$DFLTFOLDERPATH
+cp modules_disabled.txt ../$DFLTFOLDERPATH
+
+cd ../DFLTFOLDERPATH
+
+echo ""
+echo "Updating modules enabled ..."
+drush cc all
+drush dis `cat modules_disabled.txt` -y
+drush en `cat modules_enabled.txt` -y
+
+echo ""
+echo "Applying database updates ..."
+drush updb -y
+drush fra -y
+drush cc all
+
+# Pre configure settings
+##########################################################
+echo ""
+echo "Pre configure settings ..."
+
+drush vset -y site_slogan $SITESLOGAN;
+
+drush vset file_public_path "sites/$SITENAME/files"
+drush vset file_private_path "sites/$SITENAME/private"
+drush vset file_temporary_path "sites/$SITENAME/files"
+drush vset preprocess_css 0
+drush vset preprocres_js 0
+ 
+echo -e "////////////////////////////////////////////////////"
+echo -e "// Install Completed"
+echo -e "////////////////////////////////////////////////////"
